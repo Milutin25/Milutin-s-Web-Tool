41f817c281d10f4b83d570b12fd0541f2f323848
Milutin25 <stefanovicfamily@Stefanovics-iMac.local>
2024-04-23 14:53:08 +0200

    TextArea and wrong input changes

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 function processing() {
     let input = document.getElementById("textField").value;
-    let select = document.getElementById("select");
     let outputArea = document.getElementById("outputArea");
     let selectedFunction = select.options[select.selectedIndex].value;
+
+
     let result;
 
     switch (selectedFunction) {
@@ -21,29 +22,45 @@ function processing() {
         default:
             result = "Invalid function selection";
     }
-
     outputArea.innerHTML = result;
 }
 
 function factorial(num) {
-    if (num === 0 || num === 1){
-        return 1;
-    } else{
-        let sum = 1;
-        for(let i = 1; i <= num; i++) {
-        sum = sum * i;   
+    let input = document.getElementById("textField").value;
+    let outputArea = document.getElementById("outputArea");
+
+    if (isNaN(input)) {
+        document.getElementById("textField").classList.add("invalid-input");
+    return outputArea.innerHTML = "Invalid input. Please enter a valid number.";
+    } else {
+        document.getElementById("textField").classList.remove("invalid-input");
+        }
+    let sum = 1;
+    for(let i = 1; i <= num; i++) {
+        sum = sum * i;          
     }return sum;
-    }  
-}
+}  
+
 
 function reverseString(str) {
-    return str.split("").reverse().join("");
-}
+        return str.split("").reverse().join("");
+    }
+
+
 
 function demoLoop(length) {
+    let input = document.getElementById("textField").value;
+    let outputArea = document.getElementById("outputArea");
+
+    if (isNaN(input)) {
+        document.getElementById("textField").classList.add("invalid-input");
+        return outputArea.innerHTML = "Invalid input. Please enter a valid number.";
+        } else {
+        document.getElementById("textField").classList.remove("invalid-input");
+        }
     let list = "";
     for( i = 1; i <= length; i++){
-        list += i + ", ";
-        
+        list += i + ", ";   
     }return list.slice(0,-2);
-}
+    }
+
