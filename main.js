4edc3d1e764fc0d643289a74dc185af0127629a2
Milutin25 <stefanovicfamily@Stefanovics-iMac.local>
2024-04-24 12:01:34 +0200

    Loop and reverse word changes

--- main.js
+++ main.js
@@ -14,10 +14,10 @@ function processing() {
             result = reverseString(input);
             break;
         case "uppercase":
-            result = input.toUpperCase();
+            result = uppercase(input);
             break;
         case "loop":
-            result = demoLoop(parseInt(input));
+            result = demoLoop(input);
             break;
         default:
             result = "Invalid function selection";
@@ -29,7 +29,7 @@ function factorial(num) {
     let input = document.getElementById("textField").value;
     let outputArea = document.getElementById("outputArea");
 
-    if (isNaN(input)) {
+    if (isNaN(input) || input ==="") {
         document.getElementById("textField").classList.add("invalid-input");
     return outputArea.innerHTML = "Invalid input. Please enter a valid number.";
     } else {
@@ -41,26 +41,59 @@ function factorial(num) {
     }return sum;
 }  
 
-
 function reverseString(str) {
-        return str.split("").reverse().join("");
+        let input = document.getElementById("textField").value;
+        let outputArea = document.getElementById("outputArea");
+
+        if (input === ""){
+            document.getElementById("textField").classList.add("invalid-input");
+            return outputArea.innerHTML = "Invalid input. Please enter a word or sentence.";
+        }else{
+            document.getElementById("textField").classList.remove("invalid-input");
+            return str.split("").reverse().join("");
     }
-
-
+}
 
 function demoLoop(length) {
     let input = document.getElementById("textField").value;
     let outputArea = document.getElementById("outputArea");
 
-    if (isNaN(input)) {
+    if (input === "") {
         document.getElementById("textField").classList.add("invalid-input");
-        return outputArea.innerHTML = "Invalid input. Please enter a valid number.";
-        } else {
-        document.getElementById("textField").classList.remove("invalid-input");
-        }
+        return outputArea.innerHTML = "Invalid input. Please enter a valid number or a word.";
+    } else if(Number(input)){
+    document.getElementById("textField").classList.remove("invalid-input");
     let list = "";
-    for( i = 1; i <= length; i++){
-        list += i + ", ";   
-    }return list.slice(0,-2);
+        for( i = 1; i <= length; i++){
+            list += i + ", ";   
+            }
+            return list.slice(0,-2);
+    }else{
+        let loopedString;
+        for (let i = 0; i < length.length; i++){
+            loopedString = length.split(""); 
+            loopedString = loopedString.filter(char => char !== ' '); //removing white space in sentence
+        }
+        return(loopedString);
     }
+}
 
+
+
+
+
+
+function uppercase(word){
+    let input = document.getElementById("textField").value;
+    let outputArea = document.getElementById("outputArea");
+
+    if (Number(input) || input === ""){
+        document.getElementById("textField").classList.add("invalid-input");
+        return outputArea.innerHTML = "Invalid input. Please enter a word or sentence.";
+        }else{
+        document.getElementById("textField").classList.remove("invalid-input"); 
+        return word.replace(/(^|\. *)([a-z])/g, function(match) {
+            return match.toUpperCase();
+            }); 
+        }
+    }
